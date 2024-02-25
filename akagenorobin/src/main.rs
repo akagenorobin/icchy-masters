955523be3fee6ffe70d972aa109483bbb2f4b553
Hidetoshi Sugihara <sugihara.ht.0101@gmail.com>
2024-02-25 15:59:41 +0900

    なるべくソートされるように移動する

--- akagenorobin/src/main.rs
+++ akagenorobin/src/main.rs
@@ -1,6 +1,16 @@
 use proconio::input;
 
-fn solve(target: usize, m: usize, a: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+fn energy(yama: Vec<i32>) -> i32{
+    let mut e = 0;
+    for j in 0 .. yama.len() - 1 {
+        if yama[j] > yama[j + 1] {
+            e -= 1;
+        }
+    }
+    e
+}
+
+fn solve(target: usize, a: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     let mut b = a.clone();
 
 /*    for (k, c) in b.iter().enumerate() {
@@ -16,10 +26,25 @@ fn solve(target: usize, m: usize, a: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
                     b[i].pop();
                 }
                 else {
-                    let tonari: usize = (i + 1) % m;
-                    println!("{} {}", yama[j + 1], tonari + 1);
+                    let mut max_e = -300;
+                    let mut target_k = 0;
+
+                    for (k, yama2) in a.iter().enumerate() {
+                        if i == k {
+                            continue;
+                        }
+                        let mut y = yama2.clone();
+                        y.extend(&yama[j + 1..yama.len()]);
+                        let e = energy(y);
+                        if e > max_e {
+                            max_e = e;
+                            target_k = k;
+                        }
+                    }
+
+                    println!("{} {}", yama[j + 1], target_k + 1);
                     println!("{} 0", target);
-                    b[tonari].extend(&yama[j + 1..yama.len()]);
+                    b[target_k].extend(&yama[j + 1..yama.len()]);
                     for _ in j..yama.len() {
                         b[i].pop();
                     }
@@ -40,6 +65,6 @@ fn main() {
 
     let mut b = a.clone();
     for target in 1..n+1 {
-        b = solve(target, m, b);
+        b = solve(target, b);
     }
 }
