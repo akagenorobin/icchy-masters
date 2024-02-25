f02479def471c93a91af49cab8fa4b44a4cd27bc
Hidetoshi Sugihara <sugihara.ht.0101@gmail.com>
2024-02-25 15:15:27 +0900

    右に動かすソルバー AC

--- akagenorobin/src/main.rs
+++ akagenorobin/src/main.rs
@@ -3,25 +3,31 @@ use proconio::input;
 fn solve(target: usize, m: usize, a: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
     let mut b = a.clone();
 
+/*    for (k, c) in b.iter().enumerate() {
+        println!("{}: {:?}", k + 1, c);
+    }
+    println!("-----------------------");
+*/
     for (i, yama) in a.iter().enumerate() {
         for (j, hako) in yama.iter().enumerate() {
             if hako == &(target as i32) {
                 if j == yama.len() - 1 {
                     println!("{} 0", target);
+                    b[i].pop();
                 }
                 else {
                     let tonari: usize = (i + 1) % m;
-                    println!("{} {}", yama[j + 1], tonari);
-                    b[tonari].extend(&yama[j + 1..yama.len() - 1]);
-                    for _ in j + 1..yama.len() - 1 {
-                        b[j].pop();
+                    println!("{} {}", yama[j + 1], tonari + 1);
+                    println!("{} 0", target);
+                    b[tonari].extend(&yama[j + 1..yama.len()]);
+                    for _ in j..yama.len() {
+                        b[i].pop();
                     }
                 }
             }
         }
     }
 
-    println!("{:?}", b);
     b
 }
 
@@ -32,14 +38,8 @@ fn main() {
         a: [[i32; n/m]; m],
     }
 
-/*    let mut stack = Vec::new();
-    for _a in &a {
-        const v = Vec::from(&_a);
-        stack.push(v);
-    }
-*/
     let mut b = a.clone();
-    for target in 1..n {
+    for target in 1..n+1 {
         b = solve(target, m, b);
     }
 }
