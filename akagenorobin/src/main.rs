c4986dd97064f10ef2ed99490dfecca5aa5d7597
Hidetoshi Sugihara <sugihara.ht.0101@gmail.com>
2024-02-25 14:42:14 +0900

    雑なまだバグっているソルバー

--- akagenorobin/src/main.rs
+++ akagenorobin/src/main.rs
@@ -1,5 +1,30 @@
 use proconio::input;
 
+fn solve(target: usize, m: usize, a: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let mut b = a.clone();
+
+    for (i, yama) in a.iter().enumerate() {
+        for (j, hako) in yama.iter().enumerate() {
+            if hako == &(target as i32) {
+                if j == yama.len() - 1 {
+                    println!("{} 0", target);
+                }
+                else {
+                    let tonari: usize = (i + 1) % m;
+                    println!("{} {}", yama[j + 1], tonari);
+                    b[tonari].extend(&yama[j + 1..yama.len() - 1]);
+                    for _ in j + 1..yama.len() - 1 {
+                        b[j].pop();
+                    }
+                }
+            }
+        }
+    }
+
+    println!("{:?}", b);
+    b
+}
+
 fn main() {
     input! {
         n: usize,
@@ -7,9 +32,14 @@ fn main() {
         a: [[i32; n/m]; m],
     }
 
-    for i in 0..m {
-        for j in 0..n/m {
-            println!("{}", a[i][j]);
-        }
+/*    let mut stack = Vec::new();
+    for _a in &a {
+        const v = Vec::from(&_a);
+        stack.push(v);
+    }
+*/
+    let mut b = a.clone();
+    for target in 1..n {
+        b = solve(target, m, b);
     }
 }
