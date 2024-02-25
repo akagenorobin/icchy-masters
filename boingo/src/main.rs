719acab3044096fbce23fa56c3bf784573ee2794
toshahiko <04girl.u.want@gmail.com>
2024-02-25 15:45:02 +0900

    akagenorobinのコードを改良
    次の山を求める時に、一番小さな山を使うようにした。

--- boingo/src/main.rs
+++ boingo/src/main.rs
@@ -1,3 +1,60 @@
+use proconio::input;
+
+fn query_next( current: usize, a:&Vec<Vec<i32>> ) -> usize {
+    let mut result = ( 4294967294 as usize, 4294967294) ;
+    for (i, yama) in a.iter().enumerate() {
+        if i == current {
+            continue;
+        }
+
+        if  result.1 > yama.len() {
+            result = ( i , yama.len() )
+        }
+    }
+
+    result.0
+}
+
+fn solve(target: usize, m: usize, a: Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let mut b = a.clone();
+
+    /*    for (k, c) in b.iter().enumerate() {
+            println!("{}: {:?}", k + 1, c);
+        }
+        println!("-----------------------");
+    */
+    for (i, yama) in a.iter().enumerate() {
+        for (j, hako) in yama.iter().enumerate() {
+            if hako == &(target as i32) {
+                if j == yama.len() - 1 {
+                    println!("{} 0", target);
+                    b[i].pop();
+                }
+                else {
+                    let tonari: usize = query_next( i , &a );
+                    println!("{} {}", yama[j + 1], tonari + 1);
+                    println!("{} 0", target);
+                    b[tonari].extend(&yama[j + 1..yama.len()]);
+                    for _ in j..yama.len() {
+                        b[i].pop();
+                    }
+                }
+            }
+        }
+    }
+
+    b
+}
+
 fn main() {
-    println!("Hello, world!");
+    input! {
+        n: usize,
+        m: usize,
+        a: [[i32; n/m]; m],
+    }
+
+    let mut b = a.clone();
+    for target in 1..n+1 {
+        b = solve(target, m, b);
+    }
 }
