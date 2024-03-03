85345e5a0a40c80f49359ca3117f049ed0f7fdae
toshahiko <04girl.u.want@gmail.com>
2024-03-03 14:33:27 +0900

    ビジュアライザが書けた。

--- masters001/boingo/src/main.rs
+++ masters001/boingo/src/main.rs
@@ -1,28 +1,53 @@
 use rand::Rng; // randクレートからRngトレイトを使用
 use std::io;
+use proconio::input;
+
+fn solve(n: usize, v: &Vec<Vec<i32>>, h: &Vec<Vec<i32>>, a: &Vec<Vec<i32>>) -> Vec<String> {
+    let mut ans: Vec<String> = vec![];
+    ans.push(format!("{} {} {} {}", 0, 0, n - 1, n - 1));
+    ans
+}
 
 fn main() {
-    println!("Enter the size of the grid N:");
-    let mut n_str = String::new();
-    io::stdin().read_line(&mut n_str).expect("Failed to read line");
-    let n: usize = n_str.trim().parse().expect("Please type a number!");
-
-    // 壁のデータを保持するためのベクタを初期化
-    let mut walls_horizontal = vec![vec![0; n]; n + 1];
-    let mut walls_vertical = vec![vec![0; n + 1]; n];
-
-    // ランダムに壁を生成
-    let mut rng = rand::thread_rng();
-    for i in 0..n {
-        for j in 0..n {
-            if i < n - 1 {
-                walls_horizontal[i + 1][j] = rng.gen_range(0..2); // 下側の壁
-            }
-            if j < n - 1 {
-                walls_vertical[i][j + 1] = rng.gen_range(0..2); // 右側の壁
-            }
-        }
+    input! {
+        _t: usize,
+        n: usize,
+        v_input: [String; n],
+        h_input: [String; n-1],
+        a: [[i32; n]; n],
     }
+
+    let mut v: Vec<Vec<i32>> = vec![];
+    let mut h: Vec<Vec<i32>> = vec![];
+
+    for v_ in v_input {
+        let mut _v: Vec<i32> = vec![];
+        for v_char in v_.chars() {
+            _v.push(v_char as i32 - 48);
+        }
+        v.push(_v);
+    }
+
+    for h_ in h_input {
+        let mut _h: Vec<i32> = vec![];
+        for h_char in h_.chars() {
+            _h.push(h_char as i32 - 48);
+        }
+        h.push(_h);
+    }
+
+    visualize( n, v, h, a);
+
+    // let ans = crate::solve(n, v, h, a);
+
+    // for step in ans {
+    //     println!("{}", step);
+    // }
+    //
+
+}
+
+fn visualize( n: usize, v : Vec<Vec<i32>>, h : Vec<Vec<i32>>, a : Vec<Vec<i32>> ) {
 
     let digit = get_digit_count(n*n ) ;
 
@@ -30,18 +55,21 @@ fn main() {
     for i in 0..n {
         for j in 0..n {
             if  j == 0 {print!(" ") ;}
-            print!("{:digit$} ", i * n + j + 1);
-            if walls_vertical[i][j + 1] == 1 {
+            print!("{:digit$} ", a[i][j]);
+            if  j == n - 1  {continue;}
+            if v[i][j] == 1 {
                 print!("|"); // 右側の壁を表す
             } else {
                 print!(" "); // 壁がない場合は空白
             }
         }
         println!();
+
+        if  i == n - 1  {continue;}
         // 下側の壁を出力
         for j in 0..n {
             print!(" ");
-            if walls_horizontal[i + 1][j] == 1 {
+            if h[i][j] == 1 {
                 // 壁がある場合
                 for k in 0..digit {
                     print!("-");
