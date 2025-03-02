358889488a98af61902d51396ada84cf4e31b1f5
Hidetoshi Sugihara <sugihara.ht.0101@gmail.com>
2025-03-02 14:42:03 +0900

    鉱石の場所まで移動

--- masters002/akageno/src/main.rs
+++ masters002/akageno/src/main.rs
@@ -31,48 +31,81 @@ impl Board {
         let mut hole_b = (0, 0);
         let mut hole_c = (0, 0);
 
-        for i in 0..n {
-            for j in 0..n {
-                if grid[i][j] == 'A' {
+        for j in 0..n {
+            for i in 0..n {
+                if grid[j][i] == 'A' {
                     hole_a = (i, j);
                 }
-                if grid[i][j] == 'B' {
+                if grid[j][i] == 'B' {
                     hole_b = (i, j);
                 }
-                if grid[i][j] == 'C' {
+                if grid[j][i] == 'C' {
                     hole_c = (i, j);
                 }
             }
         }
-        Board {n, x, y, hole_a, hole_b, hole_c, grid}
+        Board {
+            n,
+            x,
+            y,
+            hole_a,
+            hole_b,
+            hole_c,
+            grid,
+        }
+    }
+
+    pub fn now(&mut self) -> char {
+        self.grid[self.y][self.x]
     }
 
     pub fn mv(&mut self, d: Direction) {
         match d {
-            Direction::Right => {self.x += 1},
-            Direction::Left => {self.x -= 1},
-            Direction::Up => {self.y += 1},
-            Direction::Down => {self.y -= 1},
+            Direction::Right => {
+                self.x += 1;
+                println!("1 R");
+            }
+            Direction::Left => {
+                self.x -= 1;
+                println!("1 L");
+            }
+            Direction::Up => {
+                self.y += 1;
+                println!("1 U");
+            }
+            Direction::Down => {
+                self.y -= 1;
+                println!("1 D");
+            }
         }
+        println!("")
     }
 
     pub fn carry(&mut self, d: Direction) {
         match d {
             Direction::Right => {
-                self.grid[self.x + 1][self.y] = self.grid[self.x][self.y];
-                self.grid[self.x][self.y] = '.';
-            },
+                if self.grid[self.y][self.x + 1] == '.' {
+                    self.grid[self.y][self.x + 1] = self.grid[self.y][self.x];
+                }
+                self.grid[self.y][self.x] = '.';
+            }
             Direction::Left => {
-                self.grid[self.x - 1][self.y] = self.grid[self.x][self.y];
-                self.grid[self.x][self.y] = '.';
-            },
+                if self.grid[self.y][self.x - 1] == '.' {
+                    self.grid[self.y][self.x - 1] = self.grid[self.y][self.x];
+                }
+                self.grid[self.y][self.x] = '.';
+            }
             Direction::Up => {
-                self.grid[self.x][self.y - 1] = self.grid[self.x][self.y];
-                self.grid[self.x][self.y] = '.';
-            },
+                if self.grid[self.y - 1][self.x] == '.' {
+                    self.grid[self.y - 1][self.x] = self.grid[self.y][self.x];
+                }
+                self.grid[self.y][self.x] = '.';
+            }
             Direction::Down => {
-                self.grid[self.x][self.y + 1] = self.grid[self.x][self.y];
-                self.grid[self.x][self.y] = '.';
+                if self.grid[self.y + 1][self.x] == '.' {
+                    self.grid[self.y + 1][self.x] = self.grid[self.y][self.x];
+                }
+                self.grid[self.y][self.x] = '.';
             }
         }
         self.mv(d);
@@ -95,23 +128,69 @@ fn input() -> Input {
     Input { n, m, grid }
 }
 
-fn solve (input: Input) {
-    let board = Board::new(input.n, 0, 0, input.grid);
+fn solve(input: Input) {
+    let mut board = Board::new(input.n, 0, 0, input.grid);
 
-    for _ in 0..input.n * input.n {
-        let target = (0, 0);
-        for i in 0..n {
-            for j in 0..n {
-                if board.grid[i][j] == 'A' {
+    for _ in 0..10000 {
+        println!("{:?} {:?} {:?}", board.now(), board.x, board.y);
 
+        if board.now() == '.' {
+            let mut moved = false;
+            let mut d = Direction::Right;
+
+            if board.x < input.n - 1 && !moved {
+                if board.grid[board.y][board.x + 1] == 'a' {
+                    d = Direction::Right;
+                    moved = true;
                 }
-                let target =
-
             }
-        }
+            if board.x > 0 && !moved {
+                if board.grid[board.y][board.x - 1] == 'a' {
+                    d = Direction::Left;
+                    moved = true;
+                }
+            }
+            if board.y > 0 && !moved {
+                if board.grid[board.y - 1][board.x] == 'a' {
+                    d = Direction::Up;
+                    moved = true;
+                }
+            }
+            if board.y < input.n - 1 && !moved {
+                if board.grid[board.y + 1][board.x] == 'a' {
+                    d = Direction::Down;
+                    moved = true;
+                }
+            }
 
+            if board.x < input.n - 1 && !moved {
+                if board.grid[board.y][board.x + 1] == '.' {
+                    d = Direction::Right;
+                    moved = true;
+                }
+            }
+            if board.y < input.n - 1 && !moved {
+                if board.grid[board.y + 1][board.x] == '.' {
+                    d = Direction::Down;
+                    moved = true;
+                }
+            }
+            if board.x > 0 && !moved {
+                if board.grid[board.y][board.x - 1] == '.' {
+                    d = Direction::Left;
+                    moved = true;
+                }
+            }
+            if board.y > 0 && !moved {
+                if board.grid[board.y - 1][board.x] == '.' {
+                    d = Direction::Up;
+                    moved = true;
+                }
+            }
+
+            board.mv(d);
+        }
     }
-    println!("{:?}", board);
 }
 
 fn main() {
